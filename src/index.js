5a90566a1b75410ef1bee8f23e004797f1badd39
streletss <xstreletssx@gmail.com>
2017-12-28 23:14:50 +0200

    loading styles

--- src/index.js
+++ src/index.js
@@ -1,9 +1,13 @@
 import _ from "lodash";
+import "./styles/css-style.css"
+import "./styles/sass-style.sass"
+import "./styles/scss-style.scss"
 
 function component(tagname) {
-  var element = document.createElement(tagname || 'div');
-  element.innerHTML = _.join(['Hello', 'webpack!!!'], ' ');
+  var element = document.createElement(tagname || "div");
+  element.innerHTML = _.join(["Hello", "webpack!!!"], " ");
+  element.classList.add("css","sass","scss")
   return element;
 }
 
-document.body.appendChild(component('h1'));
+document.body.appendChild(component("h1"));
--- webpack.config.js
+++ webpack.config.js
@@ -5,5 +5,24 @@ module.exports = {
   output: {
     filename: "bundle.js",
     path: path.resolve(__dirname, "dist")
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: [
+          "style-loader",
+          "css-loader",
+        ]
+      },
+      {
+        test: /\.(scss|sass)$/,
+        use: [
+          "style-loader",
+          "css-loader",
+          "sass-loader",
+        ]
+      }
+    ]
   }
 }
