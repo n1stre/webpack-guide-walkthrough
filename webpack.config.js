38f82004a9a7e40b707ff0e60e620a40cd186657
streletss <xstreletssx@gmail.com>
2017-12-29 11:32:34 +0200

    laoding fonts

--- webpack.config.js
+++ webpack.config.js
@@ -31,11 +31,25 @@ module.exports = {
             options: {
               name: "[name].[ext]",
               publicPath: "dist/",
-              outputPath: "assets/"
+              outputPath: "assets/images/"
+            }  
+          }
+        ]
+      },
+      {
+        test: /\.(woff|woff2|eot|ttf|otf)$/,
+        use: [
+          {
+            loader: "file-loader",
+            options: {
+              name: "[name].[ext]",
+              publicPath: "dist/",
+              outputPath: "assets/fonts/"
             }  
           }
         ]
       }
+
     ]
   }
 }
