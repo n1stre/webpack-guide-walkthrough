da6fea7dade0fdc7a1b6a9852b673b5a317b566b
streletss <xstreletssx@gmail.com>
2017-12-30 23:39:59 +0200

    htmlwebpackplugin setup

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,5 @@
 const path = require("path");
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
   entry: {
@@ -8,5 +9,11 @@ module.exports = {
   output: {
     filename: "[name].bundle.js",
     path: path.resolve(__dirname, "dist")
-  }
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "./index.html",
+      title: "Webpack | Output Management",
+    }),
+  ]
 }
